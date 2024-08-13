4589087e1256418340952dc7ddd9772277065b9f
Diego Fernandes <diego.schell.f@gmail.com>
2024-08-13 10:42:02 -0300

    chore: invalidate summary upon goal creation

--- src/components/create-goal.tsx
+++ src/components/create-goal.tsx
@@ -1,153 +1,152 @@
-import { X } from 'lucide-react'
-import { Button } from './ui/button'
+import { X } from "lucide-react";
+import { Button } from "./ui/button";
 import {
-  RadioGroup,
-  RadioGroupIndicator,
-  RadioGroupItem,
-} from './ui/radio-group'
+	RadioGroup,
+	RadioGroupIndicator,
+	RadioGroupItem,
+} from "./ui/radio-group";
 import {
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-} from './ui/dialog'
-import { Input } from './ui/input'
-import { Label } from './ui/label'
-import { z } from 'zod'
-import { Controller, useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { createGoal } from '../http/create-goal'
-import { toast } from 'sonner'
-import { useQueryClient } from '@tanstack/react-query'
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogTitle,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { z } from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createGoal } from "../http/create-goal";
+import { toast } from "sonner";
+import { useQueryClient } from "@tanstack/react-query";
 
 const createGoalSchema = z.object({
-  title: z.string().min(1, 'Informe a atividade que deseja praticar'),
-  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
-})
+	title: z.string().min(1, "Informe a atividade que deseja praticar"),
+	desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+});
 
-type CreateGoalSchema = z.infer<typeof createGoalSchema>
+type CreateGoalSchema = z.infer<typeof createGoalSchema>;
 
 export function CreateGoal() {
-  const queryClient = useQueryClient()
+	const queryClient = useQueryClient();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    control,
-    reset,
-  } = useForm<CreateGoalSchema>({
-    resolver: zodResolver(createGoalSchema),
-  })
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		control,
+		reset,
+	} = useForm<CreateGoalSchema>({
+		resolver: zodResolver(createGoalSchema),
+	});
 
-  async function handleCreateGoal({
-    title,
-    desiredWeeklyFrequency,
-  }: CreateGoalSchema) {
-    try {
-      await createGoal({
-        title,
-        desiredWeeklyFrequency,
-      })
+	async function handleCreateGoal({
+		title,
+		desiredWeeklyFrequency,
+	}: CreateGoalSchema) {
+		try {
+			await createGoal({
+				title,
+				desiredWeeklyFrequency,
+			});
 
-      reset()
+			reset();
 
-      queryClient.invalidateQueries({
-        queryKey: ['pending-goals'],
-      })
+			queryClient.invalidateQueries({ queryKey: ["pending-goals"] });
+			queryClient.invalidateQueries({ queryKey: ["summary"] });
 
-      toast.success('Meta criada com sucesso!')
-    } catch {
-      toast.error('Erro ao criar a meta, tente novamente!')
-    }
-  }
+			toast.success("Meta criada com sucesso!");
+		} catch {
+			toast.error("Erro ao criar a meta, tente novamente!");
+		}
+	}
 
-  return (
-    <DialogContent>
-      <div className="flex flex-col gap-6 h-full">
-        <div className="space-y-3">
-          <div className="flex items-center justify-between">
-            <DialogTitle>Cadastrar meta</DialogTitle>
+	return (
+		<DialogContent>
+			<div className="flex flex-col gap-6 h-full">
+				<div className="space-y-3">
+					<div className="flex items-center justify-between">
+						<DialogTitle>Cadastrar meta</DialogTitle>
 
-            <DialogClose>
-              <X className="size-5 text-zinc-600" />
-            </DialogClose>
-          </div>
+						<DialogClose>
+							<X className="size-5 text-zinc-600" />
+						</DialogClose>
+					</div>
 
-          <DialogDescription>
-            Adicione atividades que te fazem bem e que você quer continuar
-            praticando toda semana.
-          </DialogDescription>
-        </div>
+					<DialogDescription>
+						Adicione atividades que te fazem bem e que você quer continuar
+						praticando toda semana.
+					</DialogDescription>
+				</div>
 
-        <form
-          onSubmit={handleSubmit(handleCreateGoal)}
-          className="flex-1 flex flex-col justify-between"
-        >
-          <div className="space-y-6">
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="title">Qual a atividade?</Label>
+				<form
+					onSubmit={handleSubmit(handleCreateGoal)}
+					className="flex-1 flex flex-col justify-between"
+				>
+					<div className="space-y-6">
+						<div className="flex flex-col gap-2">
+							<Label htmlFor="title">Qual a atividade?</Label>
 
-              <Input
-                id="title"
-                autoFocus
-                placeholder="Praticar exercícios, meditar, etc..."
-                {...register('title')}
-              />
+							<Input
+								id="title"
+								autoFocus
+								placeholder="Praticar exercícios, meditar, etc..."
+								{...register("title")}
+							/>
 
-              {errors.title && (
-                <p className="text-sm text-red-400">{errors.title.message}</p>
-              )}
-            </div>
+							{errors.title && (
+								<p className="text-sm text-red-400">{errors.title.message}</p>
+							)}
+						</div>
 
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="desiredWeeklyFrequency">
-                Quantas vezes na semana?
-              </Label>
+						<div className="flex flex-col gap-2">
+							<Label htmlFor="desiredWeeklyFrequency">
+								Quantas vezes na semana?
+							</Label>
 
-              <Controller
-                control={control}
-                name="desiredWeeklyFrequency"
-                defaultValue={5}
-                render={({ field }) => {
-                  return (
-                    <RadioGroup
-                      value={String(field.value)}
-                      onValueChange={field.onChange}
-                    >
-                      {Array.from({ length: 7 }).map((_, i) => {
-                        const frequency = String(i + 1)
+							<Controller
+								control={control}
+								name="desiredWeeklyFrequency"
+								defaultValue={5}
+								render={({ field }) => {
+									return (
+										<RadioGroup
+											value={String(field.value)}
+											onValueChange={field.onChange}
+										>
+											{Array.from({ length: 7 }).map((_, i) => {
+												const frequency = String(i + 1);
 
-                        return (
-                          <RadioGroupItem key={i} value={frequency}>
-                            <RadioGroupIndicator />
-                            <span className="text-zinc-300 text-sm font-medium leading-none">
-                              {frequency}x na semana
-                            </span>
-                            <span className="text-lg leading-none">🥱</span>
-                          </RadioGroupItem>
-                        )
-                      })}
-                    </RadioGroup>
-                  )
-                }}
-              />
-            </div>
-          </div>
+												return (
+													<RadioGroupItem key={i} value={frequency}>
+														<RadioGroupIndicator />
+														<span className="text-zinc-300 text-sm font-medium leading-none">
+															{frequency}x na semana
+														</span>
+														<span className="text-lg leading-none">🥱</span>
+													</RadioGroupItem>
+												);
+											})}
+										</RadioGroup>
+									);
+								}}
+							/>
+						</div>
+					</div>
 
-          <div className="flex items-center gap-3 mt-auto">
-            <DialogClose asChild>
-              <Button variant="secondary" className="flex-1">
-                Fechar
-              </Button>
-            </DialogClose>
+					<div className="flex items-center gap-3 mt-auto">
+						<DialogClose asChild>
+							<Button variant="secondary" className="flex-1">
+								Fechar
+							</Button>
+						</DialogClose>
 
-            <Button type="submit" className="flex-1">
-              Salvar
-            </Button>
-          </div>
-        </form>
-      </div>
-    </DialogContent>
-  )
+						<Button type="submit" className="flex-1">
+							Salvar
+						</Button>
+					</div>
+				</form>
+			</div>
+		</DialogContent>
+	);
 }
