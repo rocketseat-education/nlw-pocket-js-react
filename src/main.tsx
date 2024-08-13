114ba204660f4da072358fcf0d9566c39931a7f7
Diego Fernandes <diego.schell.f@gmail.com>
2024-08-13 10:42:52 -0300

    chore: move toast to bottom left

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
-import './index.css'
+import "./index.css";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './app'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Toaster } from 'sonner'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
 
-      <Toaster />
-    </QueryClientProvider>
-  </StrictMode>
-)
+			<Toaster position="bottom-left" richColors />
+		</QueryClientProvider>
+	</StrictMode>,
+);
